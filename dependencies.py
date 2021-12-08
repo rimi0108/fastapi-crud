b93175c265a7cf14d03d73bd9bed0b92459626cb
rimi <73830753+rimi0108@users.noreply.github.com>
2021-12-08 17:17:12 +0900

    Feat: user authorization
    - token 발급 후 유저 인가 후 api 사용 가능하게 구현

--- dependencies.py
+++ dependencies.py
@@ -7,18 +7,16 @@ from jose import JWTError, jwt
 
 from passlib.context import CryptContext
 
-from pydantic import BaseModel
 
-from database.schemas import TokenData
+from database.schemas import TokenData, User
 from database.database import SessionLocal
-from Presenter.user_presenter import get_user
+from Presenter.user_presenter import get_user_by_name
 
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -34,6 +32,21 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(db, username: str, password: str):
+    user = get_user_by_name(db, username)
+    if not user:
+        return False
+    return user
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
@@ -57,20 +70,20 @@ async def get_current_user(
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: str = payload.get("sub")
-        if user_id is None:
+        username: str = payload.get("sub")
+        if username is None:
             raise credentials_exception
-        token_data = TokenData(user_id=user_id)
+        token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(db, user_id=token_data.user_id)
+    user = get_user_by_name(db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
 
 
-# async def get_current_active_user(current_user: User = Depends(get_current_user)):
-#     if current_user.disabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.is_active == False:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
